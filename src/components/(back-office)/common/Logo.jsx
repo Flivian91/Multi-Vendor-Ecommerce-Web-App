80692208771f4828e248e54d0f14dc56661730d0
Flivian91 <fliviandev@gmail.com>
2025-08-28 17:31:04 +0300

    commit

---
+++ src/components/(back-office)/common/Logo.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+function Logo() {
+  return (
+    <Link href={"#"} className="py-4">
+      <Image
+        src={"/Hekto.png"}
+        alt="Eccommerc logo"
+        width={100}
+        height={100}
+        className=" ml-4"
+      />
+    </Link>
+  );
+}
+
+export default Logo;
