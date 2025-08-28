102bc44fdb777183ed195f42a3e6dd378a81e5e7
Flivian91 <fliviandev@gmail.com>
2025-08-28 12:45:23 +0300

    added shadcn

--- src/components/(back-office)/Navbar.jsx
+++ src/components/(back-office)/Navbar.jsx
@@ -1,9 +1,10 @@
 import { Bell, Menu, Sun, User } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 function Navbar() {
   return (
-    <div className="flex items-center justify-between bg-slate-800 text-slate-50 h-12 px-4 py-2 fixed top-0 w-full z-50">
+    <div className="flex items-center justify-between bg-slate-800  h-16 px-4 py-2 text-green-500 fixed top-0 w-full z-10 pl-60">
       {/* Icon */}
       <button>
         <Menu />
@@ -13,11 +14,28 @@ function Navbar() {
         <button>
           <Sun />
         </button>
-        <button>
+
+        <button
+          type="button"
+          className="relative inline-flex items-center p-3 text-sm font-medium text-center bg-transparent rounded-lg  focus:outline-none "
+        >
           <Bell />
+          <span className="sr-only">Notifications</span>
+          <div className="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500  rounded-full top-0 end-6 dark:border-gray-900">
+            20
+          </div>
         </button>
+
+        {/* <button>
+          <Bell />
+        </button> */}
         <button>
-          <User />
+          <Image
+            width={200}
+            height={200}
+            src={"/image.png"}
+            className="w-8 h-8 rounded-full"
+          />
         </button>
       </div>
     </div>
